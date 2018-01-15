d85f0b077b0ce386c6766dd76897073aa8594b03
Alexis Auriac <aauriac@linux.home>
2018-01-15 01:38:00 +0100

    can solve with secant version

--- solve.py
+++ solve.py
@@ -9,7 +9,7 @@ def bisec(coef, prec, limit):
     x2 = 1
     xm = float()
 
-    while (1):
+    while 1:
         xm = (x1 + x2) / 2
         print(("x = %.*f" % (prec, xm)).rstrip('0'))
         if abs(pol.compute(coef, xm)) < limit:
@@ -26,19 +26,35 @@ def newton(coef, prec, limit):
     max = 20
     iter = 0
     derived = pol.derive(coef)
-    val = 0.5
-    val_next = float()
+    xn = 0.5
+    xn1 = float()
 
     print("x = 0.5")
     while 1:
-        val_next = val - pol.compute(coef, val) / pol.compute(derived, val)
-        print(("x = %.*f" % (prec, val_next)).rstrip('0'))
-        if abs(pol.compute(coef, val_next)) < limit:
+        xn1 = xn - pol.compute(coef, xn) / pol.compute(derived, xn)
+        print(("x = %.*f" % (prec, xn1)).rstrip('0'))
+        if abs(pol.compute(coef, xn1)) < limit:
             return 1
         if iter > max:
             return 0
         iter += 1
-        val = val_next
+        xn = xn1
 
-def secant(coef, prec):
+def secant(coef, prec, limit):
+    max = 100
+    iter = 0
+    xn_1 = 0
+    xn = 1
+    xn1 = 0
+
+    while 1:
+        xn1 = xn - (pol.compute(coef, xn) * (xn - xn_1)) / (pol.compute(coef, xn) - pol.compute(coef, xn_1))
+        print(("x = %.*f" % (prec, xn1)).rstrip('0'))
+        if abs(pol.compute(coef, xn1)) < limit:
+            return 1
+        if iter > max:
+            return 0
+        iter += 1
+        xn_1 = xn
+        xn = xn1
     return 0
