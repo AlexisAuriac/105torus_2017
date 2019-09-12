c2c4b72acd52703eb266b5276c736aadf62ce078
alexis <alexis.auriac@epitech.eu>
2019-09-12 13:49:11 +0200

    Modified error handling

--- error.py
+++ error.py
@@ -2,7 +2,7 @@ def error(arg):
     if len(arg) is not 7:
         return (84)
     for i in arg:
-        if i.isnumeric() is False and i[0] is not '-':
+        if i.isnumeric() is False or i[0] is '-':
             return (84)
     for i, j in enumerate(arg):
         arg[i] = int(j)
